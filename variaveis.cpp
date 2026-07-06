c701942a4b9c45e95242b9b24b48b21b1431cf5f
victorhugofaria <victorhugo2023faria@gmail.com>
2026-07-06 14:36:41 -0300

    arquivo inicial para revisão(caso necessário)

---
+++ variaveis.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+// variáveis tipos >>
+int main()
+{ // int: so aceita valores inteiros, arredonda para o mais proximo
+    // int num;
+    // cin >> num;
+    // cout << num << endl;
+    //-----------------------------------------------------------------
+
+    // float: aceita qualer valor tanto decimal quanto inteiro
+    // float saldo;
+    // cin >> saldo;
+    // cout << "o valor que preciso e de: " << saldo * 10 / 100;
+    //---------------------------------------------------------------
+
+    // double: mesmo coisa que o float, mas com maior precisão e maior capacidade de armazenamento
+    // double temperatura;
+    // cin >> temperatura;
+    // cout << "a temperatura da regiao e: " << temperatura;
+    //---------------------------------------------------------------
+
+    // char: aceita intercalações entre valores inteiros e caracteres
+    // char senha;
+    // cin >> senha;
+    // cout << "sua senha e: ***********";
+    //---------------------------------------------------------------
+
+    // bool: aceita apenas os valores true ou false
+    // bool senha = true;
+    // cout << senha;
+    //---------------------------------------------------------------
+
+    // string: aceita uma sequência de caracteres(letras, números, símbolos...), mas não aceita valores fora de aspas duplas
+    // string frase = " a soberba precede a queda";
+    // cout << frase;
+    //---------------------------------------------------------------
+
+    return 0;
+};
